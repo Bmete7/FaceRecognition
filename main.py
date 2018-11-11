550b6ffa888223c8b35d9725699ee4addb6edfeb
Burak Mete <burak.mete11@gmail.com>
2018-11-11 16:01:15 +0300

    pyqt needed

--- main.py
+++ main.py
@@ -4,69 +4,68 @@ Created on Sat Nov 10 19:54:52 2018
 
 @author: BurakBey
 """
+import numpy
 
 from model import create_model
 from keras import backend as K
 from keras.models import Model
 from keras.layers import Input, Layer
 import tensorflow as tf
-nn4_small2 = create_model()
-
-in_a = Input(shape=(3, 96, 96))
-in_p = Input(shape=(3, 96, 96))
-in_n = Input(shape=(3, 96, 96))
-
-emb_a = nn4_small2(in_a)
-emb_p = nn4_small2(in_p)
-emb_n = nn4_small2(in_n)
-
-
-
-class TripletLossLayer(Layer):
-    def __init__(self, alpha, **kwargs):
-        self.alpha = alpha
-        super(TripletLossLayer, self).__init__(**kwargs)
-    
-    def triplet_loss(self, inputs):
-        a, p, n = inputs
-        p_dist = K.sum(K.square(a-p), axis=-1)
-        n_dist = K.sum(K.square(a-n), axis=-1)
-        return K.sum(K.maximum(p_dist - n_dist + self.alpha, 0), axis=0)
-    
-    def call(self, inputs):
-        loss = self.triplet_loss(inputs)
-        self.add_loss(loss)
-        return loss
-
-# Layer that computes the triplet loss from anchor, positive and negative embedding vectors
-triplet_loss_layer = TripletLossLayer(alpha=0.2, name='triplet_loss_layer')([emb_a, emb_p, emb_n])
-
-# Model that can be trained with anchor, positive negative images
-nn4_small2_train = Model([in_a, in_p, in_n], triplet_loss_layer)
-
-from data import triplet_generator
-
-# triplet_generator() creates a generator that continuously returns 
-# ([a_batch, p_batch, n_batch], None) tuples where a_batch, p_batch 
-# and n_batch are batches of anchor, positive and negative RGB images 
-# each having a shape of (batch_size, 96, 96, 3).
-generator = triplet_generator() 
-
-nn4_small2_train.compile(loss=None, optimizer='adam')
-
-nn4_small2_train.fit_generator(generator, epochs=10, steps_per_epoch=100)
-
+#nn4_small2 = create_model()
+#
+#in_a = Input(shape=(3, 96, 96))
+#in_p = Input(shape=(3, 96, 96))
+#in_n = Input(shape=(3, 96, 96))
+#
+#emb_a = nn4_small2(in_a)
+#emb_p = nn4_small2(in_p)
+#emb_n = nn4_small2(in_n)
+#
+#
+#
+#class TripletLossLayer(Layer):
+#    def __init__(self, alpha, **kwargs):
+#        self.alpha = alpha
+#        super(TripletLossLayer, self).__init__(**kwargs)
+#    
+#    def triplet_loss(self, inputs):
+#        a, p, n = inputs
+#        p_dist = K.sum(K.square(a-p), axis=-1)
+#        n_dist = K.sum(K.square(a-n), axis=-1)
+#        return K.sum(K.maximum(p_dist - n_dist + self.alpha, 0), axis=0)
+#    
+#    def call(self, inputs):
+#        loss = self.triplet_loss(inputs)
+#        self.add_loss(loss)
+#        return loss
+#
+## Layer that computes the triplet loss from anchor, positive and negative embedding vectors
+#triplet_loss_layer = TripletLossLayer(alpha=0.2, name='triplet_loss_layer')([emb_a, emb_p, emb_n])
+#
+## Model that can be trained with anchor, positive negative images
+#nn4_small2_train = Model([in_a, in_p, in_n], triplet_loss_layer)
+#
+#from data import triplet_generator
+#
+## triplet_generator() creates a generator that continuously returns 
+## ([a_batch, p_batch, n_batch], None) tuples where a_batch, p_batch 
+## and n_batch are batches of anchor, positive and negative RGB images 
+## each having a shape of (batch_size, 96, 96, 3).
+#generator = triplet_generator() 
+#
+#nn4_small2_train.compile(loss=None, optimizer='adam')
+#
+#nn4_small2_train.fit_generator(generator, epochs=10, steps_per_epoch=100)
+#
 
 ## or we can use a pre trained modal
 nn4_small2_pretrained = create_model()
 nn4_small2_pretrained.load_weights('weights/nn4.small2.v1.h5')
 
-
-for layer in nn4_small2_train.layers:
-    print(layer.get_output_at(0).get_shape().as_list())
+nn4_small2_pretrained.summary()
+#for layer in nn4_small2_train.layers:
+#    print(layer.get_output_at(0).get_shape().as_list())
     
-myModal = nn4_small2_train.get_layer(index=-2)
-myModal.summary()
 
 import numpy as np
 import os.path
@@ -102,7 +101,7 @@ import matplotlib.patches as patches
 
 from align import AlignDlib
 
-%matplotlib inline
+#%matplotlib inline
 
 def load_image(path):
     img = cv2.imread(path, 1)
@@ -113,27 +112,27 @@ def load_image(path):
 # Initialize the OpenFace face alignment utility
 alignment = AlignDlib('models/landmarks.dat')
 
-# Load an image of Jacques Chirac
-jc_orig = load_image('model.png')
-
-# Detect face and return bounding box
-bb = alignment.getLargestFaceBoundingBox(jc_orig)
-
-# Transform image using specified face landmark indices and crop image to 96x96
-jc_aligned = alignment.align(96, jc_orig, bb, landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
-
-# Show original image
-plt.subplot(131)
-plt.imshow(jc_orig)
-
-# Show original image with bounding box
-plt.subplot(132)
-plt.imshow(jc_orig)
-plt.gca().add_patch(patches.Rectangle((bb.left(), bb.top()), bb.width(), bb.height(), fill=False, color='red'))
-
-# Show aligned image
-plt.subplot(133)
-plt.imshow(jc_aligned);
+## Load an image of Jacques Chirac
+#jc_orig = load_image('model.png')
+#
+## Detect face and return bounding box
+#bb = alignment.getLargestFaceBoundingBox(jc_orig)
+#
+## Transform image using specified face landmark indices and crop image to 96x96
+#jc_aligned = alignment.align(96, jc_orig, bb, landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
+#
+## Show original image
+#plt.subplot(131)
+#plt.imshow(jc_orig)
+#
+## Show original image with bounding box
+#plt.subplot(132)
+#plt.imshow(jc_orig)
+#plt.gca().add_patch(patches.Rectangle((bb.left(), bb.top()), bb.width(), bb.height(), fill=False, color='red'))
+#
+## Show aligned image
+#plt.subplot(133)
+#plt.imshow(jc_aligned);
 
 
 def align_image(img):
@@ -143,21 +142,25 @@ def align_image(img):
 
 embedded = np.zeros((metadata.shape[0], 128))
 
-for i, m in enumerate(metadata):
-    img = load_image(m.image_path())
-    img = align_image(img)
-    # scale RGB values to interval [0,1]
-    img = (img / 255.).astype(np.float32)
-    # obtain embedding vector for image
-    embedded[i] = nn4_small2_pretrained.predict(np.expand_dims(img, axis=0))[0]
-    
-    
+img = load_image(str(metadata[19]))
+len(img)
+if(img):
+    print('lala')
+img = align_image(img)
+img.size()
+
+# scale RGB values to interval [0,1]
+img = (img / 255.).astype(np.float32)
+# obtain embedding vector for image
+embedded[0] = nn4_small2_pretrained.predict(np.expand_dims(img, axis=0))[0]
+print(img)
 def distance(emb1, emb2):
     return np.sum(np.square(emb1 - emb2))
 
 def show_pair(idx1, idx2):
     plt.figure(figsize=(8,3))
     plt.suptitle(f'Distance = {distance(embedded[idx1], embedded[idx2]):.2f}')
+    print(distance(embedded[idx1], embedded[idx2]))
     plt.subplot(121)
     plt.imshow(load_image(metadata[idx1].image_path()))
     plt.subplot(122)
@@ -165,4 +168,39 @@ def show_pair(idx1, idx2):
 
 show_pair(2, 3)
 show_pair(2, 12)
+    
+metadata[4]
+for i, m in enumerate(metadata):
+    img = load_image(str(metadata[i]))
+    img = align_image(img)
+    try:
+        if(img.all() != None):
+            img = (img / 255.).astype(np.float32)
+            embedded[i] = nn4_small2_pretrained.predict(np.expand_dims(img, axis=0))[0]
+    except:
+        print('no face here')
+    
+    
+def distance(emb1, emb2):
+    return np.sum(np.square(emb1 - emb2))
+
+def show_pair(idx1, idx2):
+    plt.figure(figsize=(8,3))
+    plt.suptitle(f'Difference Measure = {distance(embedded[idx1], embedded[idx2]):.2f}')
+    plt.subplot(121)
+    plt.imshow(load_image(metadata[idx1].image_path()))
+    plt.subplot(122)
+    plt.imshow(load_image(metadata[idx2].image_path()));    
+
+
+def check_pair(idx1, idx2):
+    dif = distance(embedded[idx1], embedded[idx2])
+    if(dif<= 0.3):
+        print('Same Person')
+    else:
+        print('Different person')
+        
+show_pair(3, 4)
+show_pair(2, 12)
+show_pair(48, 10489)
     
